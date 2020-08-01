4b86ea6657baa6ad0cc655ba5824fb111ff4bc19
Ivane <ivanvozny@gmail.com>
2020-08-01 22:15:57 +0300

    обавил яндекс тест

--- src/test/java/tests/YandexTests.java
+++ src/test/java/tests/YandexTests.java
@@ -8,13 +8,13 @@ import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-@Tag("google_tests")
-class GoogleTest {
+@Tag("yandex_tests")
+class YandexTests {
     @Test
-    void selenideSearchTest() {
+    void selenideSearchYandexTest() {
 
         // Открыть google
-        open("https://google.com");
+        open("https://ya.ru");
 
         // Ввести Selenide в поиск
         $(byName("q")).setValue("Selenide").pressEnter();

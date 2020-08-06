29ad372b07d984cb8494ff0ec797393be3c87917
Ivane <ivanvozny@gmail.com>
2020-08-06 22:06:33 +0300

    Добавил тест l2t2

---
+++ src/test/java/tests/L2T3.java
@@ -0,0 +1,23 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+@Tag("l2t3")
+public class L2T3 {
+    @Test
+    void selenideSearchYaTests() {
+        Configuration.holdBrowserOpen = true;
+
+        open("https://ya.ru");
+        $(byName("text")).setValue("столетие ситроен");
+
+    }
+
+}

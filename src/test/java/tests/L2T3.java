b56756c09379c840f4e1b1fd2f89d78c77d1149f
Ivane <ivanvozny@gmail.com>
2020-08-07 20:59:05 +0300

    готово на проверку

--- src/test/java/tests/L2T3.java
+++ src/test/java/tests/L2T3.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,7 +16,9 @@ public class L2T3 {
         Configuration.holdBrowserOpen = true;
 
         open("https://ya.ru");
-        $(byName("text")).setValue("столетие ситроен");
+        $(byName("text")).setValue("столетие ситроен").pressEnter();
+        $("html").shouldHave(text("drive2"));
+
 
     }
 
